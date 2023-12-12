225d92e0e3fda4c963d24ebef778b8bc36423c6a
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-12-12 04:20:52 -0800

    Task 3

--- 0.rb
+++ 0.rb
@@ -5,8 +5,19 @@ class Person
   def initialize(name)
     @name = name
   end
+
+## defining the setter to be able to update the name atrribute
+  def name=(new_name)
+    @name = new_name
+  end
+
 end
 
 p = Person.new('L. Ron')
+puts p.name
+
+## updating the name using different name
+p.name = 'Hola mis amigos'
+## printing the new name assigned
 puts p.name
 
