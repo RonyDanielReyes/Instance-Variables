735004f45a49af2e293614b67a9a7a7b62e9db78
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-12-12 04:11:46 -0800

    Task 2

--- 0.rb
+++ 0.rb
@@ -1,18 +1,12 @@
 class Person
+## the attr_reader creates the getter method for name in just 1 line so it can be accesible
+  attr_reader :name
 
   def initialize(name)
     @name = name
   end
-
-  ## define a getter (accessors) to bring the variable name
-  ## we create getters cause instance variables can not be directly accessed  
-  def name
-
-    @name
-
-  end 
-
 end
 
 p = Person.new('L. Ron')
 puts p.name
+
