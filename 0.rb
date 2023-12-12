98493a8c21719ce04190a7053cc2f2b96a62ce89
RonyDanielReyes <rony1daniel2reyes3lopez4@gmail.com>
2023-12-12 04:06:59 -0800

    task 1

--- 0.rb
+++ 0.rb
@@ -4,6 +4,14 @@ class Person
     @name = name
   end
 
+  ## define a getter (accessors) to bring the variable name
+  ## we create getters cause instance variables can not be directly accessed  
+  def name
+
+    @name
+
+  end 
+
 end
 
 p = Person.new('L. Ron')
